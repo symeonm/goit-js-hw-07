b34fed51a6bcb800c093c6567250c5b473cc56bc
Symeon Melnychuk <m.simeon998@gmail.com>
2023-04-05 12:39:57 +0300

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+gallery.addEventListener('click', onClick);
+
+
+function createGallery(){
+    
+    let markup = "";
+    galleryItems.forEach(elem => {
+        markup += `<li class = "gallery__item gallery__image:hover"><a class = "gallery__link" href = ${elem.original}>
+        <img class = "gallery__image" src = ${elem.preview} data-source = ${elem.original} alt = ${elem.description}></a></li>`}
+    )
+    gallery.insertAdjacentHTML('afterbegin', markup)
+    // console.log(markup)
+}
+createGallery()
+
+function onClick(evt){
+    evt.preventDefault()
+
+    if (evt.target.classList.contains('gallery__item')) {
+        return
+    }
+    
+    const originalImage = evt.target.dataset.source
+    
+    const instance = basicLightbox.create(`<img src = ${originalImage}>`)
+
+    instance.show()
+    
+    document.addEventListener('keydown', closeModal)
+
+    function closeModal(evt){
+    console.log(evt)
+    if (evt.key === 'Escape') {
+        instance.close(() => document.removeEventListener('keydown', closeModal))
+     }
+    }
+
+}
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,39 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+
+
+function createGallery(){
+    
+    let markup = "";
+    galleryItems.forEach(elem => {
+        markup += `<li class = "gallery__item gallery__image:hover"><a class = "gallery__link" href = ${elem.original}>
+        <img class = "gallery__image" src = ${elem.preview} data-source = ${elem.original} alt = ${elem.description}></a></li>`}
+    )
+    gallery.insertAdjacentHTML('afterbegin', markup)
+   
+}
+createGallery()
+
+
+    var lightbox = new SimpleLightbox('.gallery a', { 
+        captionsData: 'alt', 
+        captionDelay: 250
+    });
+
+   
+    document.addEventListener('keydown', closeModal)
+
+    function closeModal(evt){
+    console.log(evt)
+    if (evt.key === 'Escape') {
+        document.removeEventListener('keydown', closeModal)
+     }
+    }
+
+
+
+
+
